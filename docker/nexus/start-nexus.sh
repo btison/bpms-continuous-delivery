f8fbcfd17dadd0e356a28d379b68c5f346231a1f
Bernard Tison <bernard.tison@gmail.com>
2015-11-13 09:31:55 +0100

    nexus image: create data directory if not does not exist

--- docker/nexus/start-nexus.sh
+++ docker/nexus/start-nexus.sh
@@ -20,14 +20,14 @@ fi
 
 # configure nexus
 if [ ! -d "$NEXUS_DATA_DIR/conf" ]; then
-  mkdir $NEXUS_DATA_DIR/conf
+  mkdir -p $NEXUS_DATA_DIR/conf
   cp -r $CONFIGURATION_DIR/nexus.xml $NEXUS_DATA_DIR/conf
   VARS=( NEXUS_VERSION )
   for i in "${VARS[@]}"
   do
     sed -i "s'@@${i}@@'${!i}'" $NEXUS_DATA_DIR/conf/nexus.xml	
   done
-  chown -R jboss:jboss $NEXUS_DATA_DIR/conf 	 
+  chown -R jboss:jboss $NEXUS_DATA_DIR	 
 fi
 
 # start nexus
